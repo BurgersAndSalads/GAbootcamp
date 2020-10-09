44cefbe8d4948b6cbbbe1f06aefd1e63a275a23f
Denis <ddujota@internationalsafety.com>
2020-10-09 13:27:37 -0400

    added npm to proj

--- w04/d1/intro-to-node/first-node/main.js
+++ w04/d1/intro-to-node/first-node/main.js
@@ -4,10 +4,14 @@
 
 // console.log(n);
 
-const fs = require('fs'); // some file in the node js install called fs.js
+// const fs = require('fs'); // some file in the node js install called fs.js
 
-console.log(typeof fs);
+// console.log(typeof fs);
 
-fs.writeFile('./hello.txt', 'Hello!', function() {
-  console.log('done creating file');
-});
+// fs.writeFile('./hello.txt', 'Hello!', function() {
+//   console.log('done creating file');
+// });
+
+const daysOfWeek = require('./days-of-week');
+
+console.log(daysOfWeek);
