ee7a30234317b4263d9a3555f0531ceb45e9c82c
Denis <ddujota@internationalsafety.com>
2020-10-09 16:19:41 -0400

    finished the code along

--- w04/d1/intro-express/server.js
+++ w04/d1/intro-express/server.js
@@ -1,6 +1,7 @@
 // Load express
 const express = require('express');
 const path = require('path');
+const todoDb = require('./data/todo-db');
 
 // Create our express app
 const app = express();
@@ -18,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 // Define a "root" route directly on app
 // Tomorrow, we'll use best practice routing
 app.get('/', function(req, res) {
-  res.send('<h1>Hello Express is awesome!!!!!!</h1>');
+  res.redirect('/home');
 });
 
 app.get('/users', function(req, res) {
@@ -27,8 +28,20 @@ app.get('/users', function(req, res) {
 });
 
 app.get('/home', function(req, res) {
-  res.render('home');
+  res.render('home', { myData: 'helloWorld' });
 });
+
+app.get('/todos', function(req, res) {
+  res.render('todos/index', {
+    todos: todoDb.getAll(),
+  });
+});
+
+obj = {
+  render() {
+    return 'template';
+  },
+};
 
 // BE CAREFUL!!!!!!!!!!!
 
