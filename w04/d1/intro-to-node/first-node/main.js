eb7a6333b3ec308055475d20688868c44b0c0798
Denis <ddujota@internationalsafety.com>
2020-10-09 12:56:15 -0400

    added the fs write and created a days of qeek module

--- w04/d1/intro-to-node/first-node/main.js
+++ w04/d1/intro-to-node/first-node/main.js
@@ -1,5 +1,13 @@
-const multiply = (a, b) => a * b;
+// const multiply = (a, b) => a * b;
 
-const n = multiply(5, 8);
+// const n = multiply(5, 10);
 
-console.log(n);
+// console.log(n);
+
+const fs = require('fs'); // some file in the node js install called fs.js
+
+console.log(typeof fs);
+
+fs.writeFile('./hello.txt', 'Hello!', function() {
+  console.log('done creating file');
+});
